d18e373e7be5369306a9fd5c79e26e36a8a27140
raphael <raphabarzotto@gmail.com>
2022-01-01 18:18:12 -0300

    finaliza o dia 11.2

---
+++ 02-desenvolvimento-front-end/11-componentes-com-estado-eventos-e-formularios-com-react/02-formularios-no-react/my-form-2.0/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import FormValidation from './components/FormValidation';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <FormValidation />
+    </>
+  );
+}
+
+export default App;
